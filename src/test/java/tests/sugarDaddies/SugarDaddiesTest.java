4e6adc04bdb6abbfc31bbcf98e1288eea95aa996
IgorSologub <sologub355@gmail.com>
2022-07-27 08:36:59 +0300

    gg9

--- src/test/java/tests/sugarDaddies/SugarDaddiesTest.java
+++ src/test/java/tests/sugarDaddies/SugarDaddiesTest.java
@@ -89,6 +89,7 @@ public class SugarDaddiesTest extends BaseTest {
             basePage.nextTab();
             basePage.waitOneMinute();
             if (basePage.checkTitles(REG_TITLES)) { goodTitlesCount++; }
+            else { basePage.outputWrongTitle(); }
             basePage.waitOneSeconds();
             basePage.closeTabAndMoveToNext();
         }
